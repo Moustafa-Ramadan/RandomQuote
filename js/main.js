d9a98c2e2f177e08206ce3ecf70c2d852305c0da
Moustafa-Ramadan <mo.ramadan996@gmail.com>
2022-08-12 00:12:25 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function test()
+{
+    var quote = [
+        "'Be yourself; everyone else is already taken.'",
+        "'I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.'",
+         "'Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.'",
+         "'So many books, so little time.'",
+         " 'A room without books is like a body without a soul.'",
+         "'Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.'",
+         "'You know you're in love when you can't fall asleep because reality is finally better than your dreams.'",
+         "'You only live once, but if you do it right, once is enough.'",
+         "'Be the change that you wish to see in the world.'",
+         "'In three words I can sum up everything I've learned about life: it goes on.'",
+      ];
+      var author = [
+        "--Oscar Wilde",
+        "--Marilyn Monroe",
+        "--Albert Einstein",
+        "--Frank Zappa",
+        "--Marcus Tullius Cicero",
+        "--Bernard M. Baruch",
+        "--Dr. Seuss",
+        "--Mae West",
+        "--Mahatma Gandhi",
+        "--Robert Frost",
+      ];
+      
+      var index= Math.floor(Math.random() * quote.length);
+      var randomQuote=quote[index];
+      var randomAuthor=author[index];
+      var quoteOutput= (document.getElementById("quoteOutput").innerHTML=randomQuote);
+      var authorOutput= (document.getElementById("authorOutput").innerHTML=randomAuthor);
+    
+}
+
